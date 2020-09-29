a5e73493d6bd5802f822e9de369778d2bf23316e
uwjake <hnujzh@hotmail.com>
2020-09-29 16:01:35 -0700

    dep

--- app.js
+++ app.js
@@ -1,4 +1,41 @@
+const WEEKDAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
+class MyDate extends Date {
+  augmentNum(num) {
+    const str = num.toString()
+    return str.length < 2 ? "0" + str : str
+  }
+
+  getUTCWeekDayName() {
+    return WEEKDAYS[super.getUTCDay()]
+  }
+
+  getUTCMonth() {
+    return this.augmentNum(super.getUTCMonth())
+  }
+
+  getUTCDate() {
+    return this.augmentNum(super.getUTCDate())
+  }
+
+  getUTCHoursInAmPm() {
+    let hours = this.augmentNum(super.getUTCHours())
+    hours = hours % 12;
+    return this.augmentNum(hours ? hours : 12)
+  }
+
+  getUTCMinutes() {
+    return this.augmentNum(super.getUTCMinutes())
+  }
+
+  getUTCSeconds() {
+    return this.augmentNum(super.getUTCSeconds())
+  }
+
+  getUTCAmPm() {
+    return super.getUTCHours() >= 12 ? "PM" : "AM"
+  }
+}
 
 var app = new Vue({
     el: '#app',
@@ -9,16 +46,20 @@ var app = new Vue({
     },
     mounted() {
       M.AutoInit()
-      // this.timestamp = parseIt((new Date()).getTime() / 1000)
       this.onSubmit()
+      const tsInputField = this.$refs["timestamp_input"]
+      tsInputField.focus()
+      tsInputField.select()
     },
     methods: {
       onSubmit() {
-        const date = new Date(this.timestamp*1000)
-        this.utcTimeString = date.toUTCString()
-        this.localTimeString = date.toTimeString()
+        let date = new MyDate(this.timestamp*1000)
+        this.utcTimeString = `${date.getUTCWeekDayName()}, ${date.getUTCFullYear()}-${date.getUTCMonth()}-${date.getUTCDate()},  
+        ${date.getUTCHoursInAmPm()}:${date.getUTCMinutes()}:${date.getUTCSeconds()} ${date.getUTCAmPm()}`
+
+        date = new MyDate(date.getTime() - date.getTimezoneOffset()*60*1000)
+        this.localTimeString = `${date.getUTCWeekDayName()}, ${date.getUTCFullYear()}-${date.getUTCMonth()}-${date.getUTCDate()},  
+        ${date.getUTCHoursInAmPm()}:${date.getUTCMinutes()}:${date.getUTCSeconds()} ${date.getUTCAmPm()} ${date.toTimeString().slice(9)}`
       },
-
-
     },
 })
